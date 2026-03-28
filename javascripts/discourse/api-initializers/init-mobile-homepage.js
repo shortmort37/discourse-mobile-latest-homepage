018462578d2aec34fe08305d81fd900bd39a6d5a
Dan Morton <shortmort37@users.noreply.github.com>
2026-03-27 20:58:07 -0400

    Update init-mobile-homepage.js
    Revert to onPageChange with window.location.replace.

--- javascripts/discourse/api-initializers/init-mobile-homepage.js
+++ javascripts/discourse/api-initializers/init-mobile-homepage.js
@@ -1,14 +1,12 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("1.0", (api) => {
-  if (!window.matchMedia("(max-width: 768px)").matches) {
-    return;
-  }
-
-  api.modifyClass("route:discovery.categories", {
-    pluginId: "mobile-latest-homepage",
-    beforeModel() {
-      this.replaceWith("discovery.latest");
+  api.onPageChange((url) => {
+    if (!window.matchMedia("(max-width: 768px)").matches) {
+      return;
+    }
+    if (url === "/" || url === "/categories") {
+      window.location.replace("/latest");
     }
   });
 });
